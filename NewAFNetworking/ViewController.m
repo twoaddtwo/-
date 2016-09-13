a6f7e8008777b1b7a4d570ec485da778282469d0
13110100625 <a13110100625@buxushanchudeimac-31.local>
2016-09-13 08:25:03 +0800

    Initial Commit

---
+++ NewAFNetworking/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewAFNetworking
+//
+//  Created by 13110100625 on 16/9/13.
+//  Copyright © 2016年 13110100625. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ NewAFNetworking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewAFNetworking
+//
+//  Created by 13110100625 on 16/9/13.
+//  Copyright © 2016年 13110100625. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
